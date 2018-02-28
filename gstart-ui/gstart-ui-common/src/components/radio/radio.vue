49a5cd1ec5f1305d763bff58a72c24f21dd9c784
SELECTFROMDATABASE <839179581@qq.com>
2018-03-01 00:24:19 +0800

    更新单选框

---
+++ gstart-ui/gstart-ui-common/src/components/radio/radio.vue
@@ -0,0 +1,20 @@
+<template>
+    <label>
+      <span class="gs-radio">
+        <span class="gs-radio-inner"></span>
+        <input class="gs-radio-input" type="radio"/>
+      </span>
+      {{label}}
+    </label>
+</template>
+
+<script>
+    export default {
+      props:{label:String},
+        name: "gs-radio"
+    }
+</script>
+
+<style>
+
+</style>
