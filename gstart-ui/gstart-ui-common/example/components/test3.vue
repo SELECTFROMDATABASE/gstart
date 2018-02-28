664d45061a913934710e66e650cd4bdb0a6ebbd3
SELECTFROMDATABASE <839179581@qq.com>
2018-03-01 00:25:21 +0800

    单选框测试

---
+++ gstart-ui/gstart-ui-common/example/components/test3.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <gs-radio :label="label"></gs-radio>
+  </div>
+</template>
+<script>
+  import Radio from "../../src/components/radio/radio";
+
+  export default {
+    components: {Radio},
+    data(){return {
+      label:"test"
+    }}
+  }
+</script>
