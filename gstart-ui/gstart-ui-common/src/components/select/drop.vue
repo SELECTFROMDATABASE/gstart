e280b84ae4db196b848fc26280a976e8473a2fba
SELECTFROMDATABASE <839179581@qq.com>
2018-03-21 17:57:35 +0800

    下拉PC

--- gstart-ui/gstart-ui-common/src/components/select/drop.vue
+++ gstart-ui/gstart-ui-common/src/components/select/drop.vue
@@ -1,6 +1,8 @@
 <template>
     <div class="gs-select-dropdown">
       <ul class="gs-select-dropdown-list">
+        <li class="gs-select-item gs-select-item-selected">测试111</li>
+        <li class="gs-select-item gs-select-item-disabled">测试111</li>
         <li class="gs-select-item">测试111</li>
         <li class="gs-select-item">测试111</li>
         <li class="gs-select-item">测试111</li>
@@ -9,15 +11,17 @@
         <li class="gs-select-item">测试111</li>
         <li class="gs-select-item">测试111</li>
         <li class="gs-select-item">测试111</li>
-        <li class="gs-select-item">测试111</li>
-        <li class="gs-select-item">测试111</li>
+        <gs-option-group></gs-option-group>
       </ul>
     </div>
 </template>
 
 <script>
+    import GsOptionGroup from "./option-group";
+
     export default {
-        name: "gs-drop"
+      components: {GsOptionGroup},
+      name: "gs-drop"
     }
 </script>
 
---
+++ gstart-ui/gstart-ui-common/src/components/select/option-group.vue
@@ -0,0 +1,22 @@
+<template>
+  <li class="gs-select-group-list-warp">
+    <div class="gs-select-group-list-title">test-group</div>
+    <ul class="gs-select-group">
+        <li class="gs-select-item">test1</li>
+        <li class="gs-select-item">test2</li>
+        <li class="gs-select-item">test3</li>
+        <li class="gs-select-item">test4</li>
+        <li class="gs-select-item">test5</li>
+    </ul>
+  </li>
+</template>
+
+<script>
+    export default {
+        name: "gs-option-group"
+    }
+</script>
+
+<style>
+
+</style>
