8dcfcdbc257679a45c05d7a534fd4a3c87f0815e
SELECTFROMDATABASE <839179581@qq.com>
2018-03-22 22:37:21 +0800

    按钮样式

---
+++ gstart-ui/gstart-ui-common/src/components/button/button.vue
@@ -0,0 +1,16 @@
+<template>
+    <button class="gs-btn">
+      <i class="gs-wode gs-icon"></i>
+      <span>test</span>
+    </button>
+</template>
+
+<script>
+    export default {
+        name: "gs-button"
+    }
+</script>
+
+<style>
+
+</style>
