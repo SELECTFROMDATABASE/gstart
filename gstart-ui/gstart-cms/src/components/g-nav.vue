62a52d9ebde470ae25563b7b4df80170c15804eb
83917 <839179581@qq.com>
2018-04-18 23:13:47 +0800

    导航实现数类型

---
+++ gstart-ui/gstart-cms/src/components/g-nav.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <Menu theme="light" width="auto" v-for="item in items" >
+      <Submenu :name="item.menuNo">
+        <template slot="title">
+          <Icon type="ios-navigate"></Icon>
+          {{item.menuName}}
+        </template>
+        <g-navsubitem :items="item.childrenItems"></g-navsubitem>
+      </Submenu>
+    </Menu>
+  </div>
+</template>
+
+<script>
+    import GNavsubitem from "./g-navsubitem";
+    export default {
+        name: "g-nav",
+      components: {GNavsubitem},
+      props:["items"]
+    }
+</script>
+
+<style>
+
+</style>
