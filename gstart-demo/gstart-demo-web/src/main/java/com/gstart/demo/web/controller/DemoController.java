cd6d4d4fa6847c5de7675728471819b8f98bf018
hp <949502500@qq.com>
2019-02-26 11:48:16 +0800

    Revert "提交"
    This reverts commit 1876361

--- gstart-demo/gstart-demo-web/src/main/java/com/gstart/demo/web/controller/DemoController.java
+++ gstart-demo/gstart-demo-web/src/main/java/com/gstart/demo/web/controller/DemoController.java
@@ -1,16 +1,24 @@
 package com.gstart.demo.web.controller;
 
+import com.alibaba.dubbo.config.annotation.Reference;
 import com.gstart.common.base.BaseController;
+import com.gstart.demo.rpc.api.DemoService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "/test")
 public class DemoController extends BaseController {
+    @Reference
+    private DemoService ds;
+
     @CrossOrigin(origins = "*", maxAge = 3600)
     @RequestMapping(value = "/a", method = RequestMethod.GET)
     @ResponseBody
     public String jsp() {
-        return "error.jsp";
+
+        return ds.sayHello();
+        //return "error.jsp";
     }
 
 }
