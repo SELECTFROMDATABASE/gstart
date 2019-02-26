3a70297a51d53a48310b34c6fcd2dff3fe621a54
hp <949502500@qq.com>
2019-02-26 14:09:04 +0800

    Revert "提交"
    This reverts commit 1876361

--- gstart-demo/gstart-demo-rpc-service/src/main/java/com/gstart/demo/service/Start.java
+++ gstart-demo/gstart-demo-rpc-service/src/main/java/com/gstart/demo/service/Start.java
@@ -1,13 +1,14 @@
 package com.gstart.demo.service;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import java.io.IOException;
 
+@SpringBootApplication
 public class Start {
     public static void main(String[] args) throws IOException {
-        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("classpath*:*.xml");
-        context.start();
-        System.in.read();
+        SpringApplication.run(Start.class, args);
     }
 }
